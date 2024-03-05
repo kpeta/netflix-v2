619b0fdfe858643822b682abfdaaafe8cfdf7b27
kpeta <kristian.petanjek146@gmail.com>
2024-03-05 14:10:53 +0000

    update newUser type

--- server/actions/users.ts
+++ server/actions/users.ts
@@ -2,10 +2,12 @@
 
 import { revalidatePath } from "next/cache";
 import supabase from "../db";
+import { User } from "../db/schema";
 
 //add user to users table
 export async function createUser(name: string, password: string) {
-  const newUser = {
+  //new user object should be type User without the id (which is automatically generated in db)
+  const newUser: Omit<User, "id"> = {
     name,
     created_at: new Date(),
     password,
