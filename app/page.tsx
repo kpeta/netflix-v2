266b9da56f6adb92ef5f730ea8eb38a33dc286ee
kpeta <kristian.petanjek146@gmail.com>
2024-03-25 14:41:34 +0000

    initial homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <main>Home Page</main>;
+import { getToken } from "@/server/auth";
+import Link from "next/link";
+
+export default async function Home() {
+  const token = await getToken();
+
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <button style={{ marginRight: "34px" }}>
+        <Link href="/data">Go to data page</Link>
+      </button>
+      <button style={{ marginRight: "34px" }}>
+        <Link href="/form">Go to form page</Link>
+      </button>
+      <button>
+        <Link href="/movies">Go to movies page</Link>
+      </button>
+      <pre>{JSON.stringify(token, null, 2)}</pre>
+    </main>
+  );
 }
