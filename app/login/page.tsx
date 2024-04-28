d6b69d526117b654734c1745b55c7dd9d5d011be
kpeta <kristian.petanjek146@gmail.com>
2024-04-28 18:59:00 +0000

    added recaptcha

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,21 @@
 "use client";
 import login from "@/server/auth/login";
 import { useFormState } from "react-dom";
+import { useRef, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import { verifyCaptcha } from "@/server/actions/recaptcha";
 
 export default function Page() {
   const [state, formAction] = useFormState(login, null);
+  const recaptchaRef = useRef<ReCAPTCHA>(null);
+  const [isVerified, setIsverified] = useState<boolean>(false);
+
+  async function handleCaptchaSubmission(token: string | null) {
+    // Server function to verify captcha
+    await verifyCaptcha(token)
+      .then(() => setIsverified(true))
+      .catch(() => setIsverified(false));
+  }
 
   return (
     <>
@@ -19,7 +31,14 @@ export default function Page() {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
         <br />
-        <button>Continue</button>
+        <ReCAPTCHA
+          sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+          ref={recaptchaRef}
+          onChange={handleCaptchaSubmission}
+        />
+        <button type="submit" disabled={!isVerified}>
+          Continue
+        </button>
       </form>
     </>
   );
---
+++ server/actions/recaptcha.ts
@@ -0,0 +1,25 @@
+"use server";
+
+export async function verifyCaptcha(token: string | null): Promise<string> {
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`;
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to verify captcha");
+    }
+
+    const data = await response.json();
+
+    if (data.success) {
+      return "success!";
+    } else {
+      throw new Error("Failed Captcha");
+    }
+  } catch (error) {
+    throw new Error("Failed to verify captcha: " + error);
+  }
+}
