2b7196f43b924cc44b94cc1a12d2bbf5d819d0c9
kpeta <kristian.petanjek146@gmail.com>
2024-06-10 14:05:18 +0000

    fixed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import Footer from "@/components/Footer";
 import Header from "@/components/header/Header";
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import { Open_Sans } from "next/font/google";
+
+const font = Open_Sans({
+  weight: "500",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Netflix v2",
@@ -18,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={font.className}>
       <body style={bodyStyle}>
         <Header />
         {children}
