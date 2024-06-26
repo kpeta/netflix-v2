4c37660c5085bed93bfdbbcf881ea3919a0b1ff6
kpeta <kristian.petanjek146@gmail.com>
2024-06-26 12:58:22 +0000

    Refactor not-found component and add NotFoundIcon

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import NotFoundIcon from "@/components/icons/NotFoundIcon";
+
+const notFoundStyle: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+  fontWeight: "bold",
+  fontSize: "2.5vh",
+  whiteSpace: "nowrap",
+};
+
+const textStyle: React.CSSProperties = {};
 
 function NotFound() {
-  return <div>404 | Not Found</div>;
+  return (
+    <div style={notFoundStyle}>
+      <NotFoundIcon />
+      <div style={textStyle}> | This page could not be found</div>
+    </div>
+  );
 }
 
 export default NotFound;
---
+++ components/icons/NotFoundIcon.tsx
@@ -0,0 +1,26 @@
+const notFoundIconStyle = {
+  marginRight: "10px",
+  height: "2em",
+  width: "2em",
+};
+
+function NotFoundIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      style={notFoundIconStyle}
+    >
+      <circle cx="12" cy="12" r="10"></circle>
+      <line x1="15" y1="9" x2="9" y2="15"></line>
+      <line x1="9" y1="9" x2="15" y2="15"></line>
+    </svg>
+  );
+}
+
+export default NotFoundIcon;
