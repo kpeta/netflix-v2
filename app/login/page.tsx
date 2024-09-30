b193e55de8068ae3af3a20fb0931bc42113e5ec4
kpeta <kristian.petanjek146@gmail.com>
2024-09-30 15:48:39 +0000

    Refactor login and signup pages to check for token before rendering form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,14 @@
 import login from "@/server/auth/login";
 import AuthForm from "@/components/AuthForm";
+import { getToken } from "@/server/auth";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
-  return (
-    <AuthForm formTitle="Log In" formAction={login} buttonText="Continue" />
-  );
+export default async function LoginPage() {
+  const token = await getToken();
+
+  if (token) {
+    redirect("/");
+  }
+
+  return <AuthForm formTitle="Log In" formAction={login} buttonText="Log In" />;
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,15 @@
 import signup from "@/server/auth/signup";
 import AuthForm from "@/components/AuthForm";
+import { getToken } from "@/server/auth";
+import { redirect } from "next/navigation";
 
-export default function SignupPage() {
+export default async function SignupPage() {
+  const token = await getToken();
+
+  if (token) {
+    redirect("/");
+  }
+
   return (
     <AuthForm formTitle="Sign Up" formAction={signup} buttonText="Continue" />
   );
