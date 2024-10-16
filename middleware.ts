57158e2178bce03963e7a811ccedff62c2988a35
kpeta <kristian.petanjek146@gmail.com>
2024-10-16 11:06:53 +0000

    Refactor middleware to handle empty response

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,6 @@ import { updateToken } from "./server/auth";
 export async function middleware(request: NextRequest) {
   const response = await updateToken(request);
 
-  if (!response) return NextResponse.error();
+  if (!response) return;
   return response;
 }
