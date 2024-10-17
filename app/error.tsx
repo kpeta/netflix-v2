798898e4a63750cc8b920f022eeaf5a268cc005f
kpeta <kristian.petanjek146@gmail.com>
2024-10-17 15:04:35 +0000

    added unhandled runtime errors page

---
+++ app/error.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect } from "react";
+import { pageContainer } from "./page";
+import WarningIcon from "@/components/icons/WarningIcon";
+
+const titleContainerStyle: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  gap: "10px",
+  color: "red",
+  alignItems: "center",
+};
+
+const terminalStyle: React.CSSProperties = {
+  backgroundColor: "#2D2D2D",
+  color: "#C5C6C7",
+  fontFamily: "monospace",
+  padding: "10px",
+  borderRadius: "5px",
+  whiteSpace: "pre-wrap",
+  overflowX: "auto",
+  margin: "10px 0",
+};
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error("Global Error:", error);
+  }, [error]);
+
+  return (
+    <main>
+      <div style={pageContainer()}>
+        <div style={titleContainerStyle}>
+          <WarningIcon />
+          <h2>An unexpected error has occurred</h2>
+        </div>
+
+        <details>
+          <summary>Show Error Details</summary>
+          <div style={terminalStyle}>{error.message}</div>
+          <button onClick={() => reset()}>Retry</button>
+        </details>
+      </div>
+    </main>
+  );
+}
