e66d1581e6e9aa5050db28bb13c2363114ad3d3f
kpeta <kristian.petanjek146@gmail.com>
2024-10-25 08:12:38 +0000

    Add error handling to fetch-test-user API endpoint

--- app/api/fetch-test-user/route.ts
+++ app/api/fetch-test-user/route.ts
@@ -1,3 +1,5 @@
+// API endpoint for testing fetching a user from the database
+// CRON job calls this endpoint every day
 import supabase from "@/server/db";
 import { PostgrestError } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
@@ -10,11 +12,15 @@ export async function GET() {
       .eq("name", "test")
       .single();
 
-    if (error) console.error("Error fetching user:", error);
-    console;
+    if (error) {
+      console.error("Error fetching user:", error);
+      return NextResponse.json({ error: error }, { status: 500 });
+    }
 
     return NextResponse.json({ data });
   } catch (error) {
+    console.error("Error fetching user:", error);
+
     const postgrestError = error as PostgrestError;
     return NextResponse.json(
       { error: postgrestError.message },
